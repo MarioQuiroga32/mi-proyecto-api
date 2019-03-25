80d833c7250d7f4fbea1336388ae42b57e134829
Mario Quiroga <marioqui135@gmail.com>
2019-03-25 20:34:14 +0100

    feat: added changes on model files

--- models/pick.model.js
+++ models/pick.model.js
@@ -1,81 +1,81 @@
-const mongoose = require('mongoose');
-const constants = require('../constants')
+// const mongoose = require('mongoose');
+// const constants = require('../constants')
 
-const pickSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  stock: {
-    type: String,
-    required: 'Stock is required',
-    uppercase: true,
-    enum: constants.stocks
-  },
-  action: {
-    type: String,
-    required: 'symbol is required',
-    enum: constants.actions
-  },
-  predictedClosing: {
-    type: String,
-    required: 'Predicted closing value is required',
-  },
-  recommendation: {
-    type: String,
-    required: 'Recommended action is required',
-    enum: constants.recommendation
-  },
-  likes: [
-    {
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
-      }
-    }
-  ],
-  description: {
-    type: String,
-    required: 'Description is required',
-  },
-  comments: [
-    {
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String
-      },
-      avatar: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
-    }
-  ],
+// const pickSchema = new mongoose.Schema({
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'users'
+//   },
+//   stock: {
+//     type: String,
+//     required: 'Stock is required',
+//     uppercase: true,
+//     enum: constants.stocks
+//   },
+//   action: {
+//     type: String,
+//     required: 'symbol is required',
+//     enum: constants.actions
+//   },
+//   predictedClosing: {
+//     type: String,
+//     required: 'Predicted closing value is required',
+//   },
+//   recommendation: {
+//     type: String,
+//     required: 'Recommended action is required',
+//     enum: constants.recommendation
+//   },
+//   likes: [
+//     {
+//       user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'users'
+//       }
+//     }
+//   ],
+//   description: {
+//     type: String,
+//     required: 'Description is required',
+//   },
+//   comments: [
+//     {
+//       user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'users'
+//       },
+//       text: {
+//         type: String,
+//         required: true
+//       },
+//       name: {
+//         type: String
+//       },
+//       avatar: {
+//         type: String
+//       },
+//       date: {
+//         type: Date,
+//         default: Date.now
+//       }
+//     }
+//   ],
   
-}, {
-  timestamps: true,
-  toJSON: {
-    transform: (doc, ret) => {
-      ret.id = doc._id;
-      delete ret._id;
-      delete ret.__v;
-      delete ret.password;
-      return ret;
-    }
-  }
-});
+// }, {
+//   timestamps: true,
+//   toJSON: {
+//     transform: (doc, ret) => {
+//       ret.id = doc._id;
+//       delete ret._id;
+//       delete ret.__v;
+//       delete ret.password;
+//       return ret;
+//     }
+//   }
+// });
 
-// pickSchema.index();
+// // pickSchema.index();
 
 
-const Pick = mongoose.model('Pick', pickSchema);
-module.exports = Pick;
+// const Pick = mongoose.model('Pick', pickSchema);
+// module.exports = Pick;
--- models/user.model.js
+++ models/user.model.js
@@ -43,6 +43,79 @@ const userSchema = new mongoose.Schema(
         ref: "User"
       }
     ],
+
+    picks: [{
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      stock: {
+        type: String,
+        required: 'Stock is required',
+        uppercase: true,
+        enum: constants.stocks
+      },
+      action: {
+        type: String,
+        required: 'symbol is required',
+        enum: constants.actions
+      },
+      predictedClosing: {
+        type: String,
+        required: 'Predicted closing value is required',
+      },
+      recommendation: {
+        type: String,
+        required: 'Recommended action is required',
+        enum: constants.recommendation
+      },
+      likes: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users'
+          }
+        }
+      ],
+      description: {
+        type: String,
+        required: 'Description is required',
+      },
+      comments: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users'
+          },
+          text: {
+            type: String,
+            required: true
+          },
+          name: {
+            type: String
+          },
+          avatar: {
+            type: String
+          },
+          date: {
+            type: Date,
+            default: Date.now
+          }
+        }
+      ],
+      
+    }, {
+      timestamps: true,
+      toJSON: {
+        transform: (doc, ret) => {
+          ret.id = doc._id;
+          delete ret._id;
+          delete ret.__v;
+          delete ret.password;
+          return ret;
+        }
+      }
+    }],
     average: {
       type: Number
     },
