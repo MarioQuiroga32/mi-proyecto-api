cdefc5c69a59a611a4ffd8c1ef4200fa6274d094
Mario Quiroga <marioqui135@gmail.com>
2019-03-27 21:37:53 +0100

    feat: added middleware authorization on user routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users.controller');
+const secure = require('../middlewares/secure.mid');
 
-router.get('/', usersController.list);
-router.post('/', usersController.create);
-router.get('/:userId', usersController.get);
-router.put('/:userId', usersController.update);
+
+router.get('/', secure.isAuthenticated, usersController.list);
+router.post('/', secure.isAuthenticated,  usersController.create);
+router.get('/:userId', secure.isAuthenticated, usersController.get);
+router.put('/:userId', secure.isAuthenticated, usersController.update);
 
 
 module.exports = router;
